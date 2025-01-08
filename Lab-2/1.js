ee25aaa141eabe5e7c6f2ca9010417f351ba46f8
nikki-2201 <nikhitharachapally@gmail.com>
2025-01-08 12:03:58 +0530

    Update 1.js

--- Lab-2/1.js
+++ Lab-2/1.js
@@ -1,8 +1,8 @@
-const { colors } = require("./8")
+
 
 function mergeArrays(...arrays){
     return[].concat(...arrays)
 }
 let a=mergeArrays([1,2],[3,4],[5,6])
 console.log(a)
-let [a, b, ...] = colors;
+
